32b9668f388743b25d10598c0c509d1fcb7c2e59
Dmitry Olshansky <dmitry.olsh@gmail.com>
2014-12-10 00:28:24 +0300

    Build release by default

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 ; Simple build script that just calls dub for each sub-package
-dub build --arch=x86 dmdscript:engine
-dub build --arch=x86 dmdscript:ds
-dub build --arch=x86 dmdscript:ds-ext
+set BUILD=release
+dub build --arch=x86 --build=%BUILD% dmdscript:engine
+dub build --arch=x86 --build=%BUILD% dmdscript:ds
+dub build --arch=x86 --build=%BUILD% dmdscript:ds-ext
 copy ds\dmdscript_ds.exe dmdscript.exe
 copy ds-ext\dmdscript_ds-ext.exe dmdscript-ext.exe
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+BUILD=release # debug
 for t in dmdscript:engine dmdscript:ds dmdscript:ds-ext
 do
-    dub build --arch=x86 $t || exit 1
+    dub build --arch=x86 --build=$BUILD $t || exit 1
 done
 # copy 2 sample binaries to the root folder
 cp ds/dmdscript_ds ./dmdscript
