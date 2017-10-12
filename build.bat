020c8ac2e173428dee3fe8fc6503f9db34e42f21
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-10-12 18:22:06 +0200

    Remove the forced 32-bit target artitecture flag from the build scripts.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 ; Simple build script that just calls dub for each sub-package
 set BUILD=release
-dub build --arch=x86 --build=%BUILD% dmdscript:engine
-dub build --arch=x86 --build=%BUILD% dmdscript:ds
-dub build --arch=x86 --build=%BUILD% dmdscript:ds-ext
+dub build --build=%BUILD% dmdscript:engine
+dub build --build=%BUILD% dmdscript:ds
+dub build --build=%BUILD% dmdscript:ds-ext
 copy ds\dmdscript_ds.exe dmdscript.exe
 copy ds-ext\dmdscript_ds-ext.exe dmdscript-ext.exe
